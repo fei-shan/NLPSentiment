468b480c320f5a98e082dc14a996f3ae39be8c8c
Fei Shan <fshan@terpmail.umd.edu>
2022-04-29 15:47:06 -0400

    change dataset

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # NLPSentiment
 
-Dataset: [50k IMDb reivews](https://www.kaggle.com/datasets/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews)
+Dataset: [IMDb](https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz)
+Alternative dataset: [50k IMDb reivews](https://www.kaggle.com/datasets/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews)
+
--- config.py
+++ config.py
@@ -1,7 +1,19 @@
 import os
 PROJECT_DIR = '.'
 LOCAL_DATA_DIR = os.path.join(PROJECT_DIR, 'data')
-IMDB_CSV = os.path.join(LOCAL_DATA_DIR, 'IMDB Dataset.csv')
+IMDB_5K_CSV = os.path.join(LOCAL_DATA_DIR, 'IMDB Dataset.csv')
+IMDB_DATA_DIR = os.path.join(LOCAL_DATA_DIR, 'acllmdb')
+
+
+class Config:
+    MAX_LEN = 256
+    BATCH_SIZE = 32
+    LR = 0.001
+    VOCAB_SIZE = 30000
+    EMBED_DIM = 128
+    NUM_HEAD = 8  # used in bert model
+    FF_DIM = 128  # used in bert model
+    NUM_LAYERS = 1
 
 # from local_config import *
 
